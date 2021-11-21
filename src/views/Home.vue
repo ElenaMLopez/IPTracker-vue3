f1ef52045598b8931fb8b23acd16d74cf60d4797
elenamlopez <elena.mateos1@gmail.com>
2021-11-21 11:09:14 +0100

    Add basic libraries for project, create basic home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex flex-col h-screen max-h-screen">
+    <!-- Search results -->
+    <div class="flex justify-center relative px-4 pt-8 pb-32 bg-hero-pattern bg-cover">
+      <!-- Search input -->
+      <div class="w-full max-w-screen-sm">
+        <h1 class="text-white text-center text-3xl pb-4">IP Address Tracker</h1>
+        <div class="flex">
+          <input 
+          class="flex-1 py-3 px-2 rounded-tl-md rounded-bl-md focus:outline-none" 
+          type="text"
+          placeholder="Search for any IP addres or leave empty to get your IP info"
+          >
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
