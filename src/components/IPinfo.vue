657bccbb3c6f806f25b84ef7a75367362880acf8
elenamlopez <elena.mateos1@gmail.com>
2021-11-21 16:36:02 +0100

    Add Ip info box and map

---
+++ src/components/IPinfo.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="absolute
+    -bottom-64
+    lg: -bottom-20
+    flex
+    flex-row
+    lg: felx-row
+    bg-white
+    gap-y-4
+    gap-x-12
+    px8
+    pt-8
+    pb-12
+    rounded-md
+  ">
+  <!-- Flex items #1 -->
+    <div class="flex flex-col">
+      <h3 class="text-xs mb-2 uppercase">IP Address</h3>
+      <span class="text-2xl">8.8.8.8</span>
+    </div>
+    <!-- Flex items #2 -->
+    <div class="flex flex-col">
+      <h3 class="text-xs mb-2 uppercase">Location</h3>
+      <span class="text-2xl">Michigan</span>
+    </div>
+    <!-- Flex items #3 -->
+    <div class="flex flex-col">
+      <h3 class="text-xs mb-2 uppercase">Time Zone</h3>
+      <span class="text-2xl">UTC -07:00</span>
+    </div>
+    <!-- Flex items #4 -->
+    <div class="flex flex-col">
+      <h3 class="text-xs mb-2 uppercase">ISP</h3>
+      <span class="text-2xl">Google</span>
+    </div>
+  </div> 
+</template>
+
+<script>
+export default {
+  name: 'IPinfo'
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col h-screen max-h-screen">
     <!-- Search results -->
-    <div class="flex justify-center relative px-4 pt-8 pb-32 bg-hero-pattern bg-cover">
+    <div class="z-20 flex justify-center relative px-4 pt-8 pb-32 bg-hero-pattern bg-cover">
       <!-- Search input -->
       <div class="w-full max-w-screen-sm">
         <h1 class="text-white text-center text-3xl pb-4">IP Address Tracker</h1>
@@ -11,18 +11,54 @@
           type="text"
           placeholder="Search for any IP addres or leave empty to get your IP info"
           >
+          <i class="cursor-pointer 
+            bg-black 
+            text-white px-4 
+            rounded-tr-md 
+            rounded-br-md
+            flex
+            items-center
+            fas fa-chevron-right"
+          ></i>
         </div>
       </div>
+      <IPinfo />
     </div>
+    <!-- Map -->
+    <div id="mapid" class="h-full z-10"></div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import IPinfo from "../components/IPinfo.vue";
+import leaflet from "leaflet";
+import { onMounted } from "vue"
+
+const TOKEN = process.env.VUE_APP_TOKEN;
+console.log({TOKEN})
 
 export default {
   name: 'Home',
   components: {
+    IPinfo,
+  },
+  setup () {
+    let mymap;
+    onMounted(() => {
+      mymap = leaflet.map('mapid').setView([51.505, -0.09], 13);
+
+      leaflet.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${TOKEN}`, {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'your.mapbox.access.token'
+      }).addTo(mymap);
+
+    });
+    leaflet
   }
 }
 </script>
